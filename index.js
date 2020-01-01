9b513cc203b7858f4d63df29e3a4ae079a36fd32
Joshua Grierson <encredgaming@gmail.com>
2020-01-01 16:54:50 +0000

    [compile] - compiled ts

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+__export(require("./lib/models"));
+__export(require("./lib/services"));
+__export(require("./lib/controllers"));
+__export(require("./lib/error"));
+//# sourceMappingURL=index.js.map
