f15f2d30618bb27cddf415237a5f167ac5af8da4
Joshua Grierson <encredgaming@gmail.com>
2020-01-01 14:55:42 +0000

    [export] - added index to root exports

---
+++ index.ts
@@ -0,0 +1,23 @@
+import {
+    ServiceStatus,
+    ServiceMethod,
+    RedisReply,
+    ShopifyProductBase,
+    Services,
+    ResponseOutput
+} from "./lib/models";
+import { Service } from "./lib/services";
+import { Controller } from "./lib/controllers";
+import ServiceError from "./lib/error";
+
+export {
+    ServiceStatus,
+    ServiceMethod,
+    RedisReply,
+    ShopifyProductBase,
+    Services,
+    ResponseOutput,
+    Service,
+    Controller,
+    ServiceError
+}
