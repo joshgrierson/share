e292fb21e803e018f3c707ea213e06f2fe8dabfe
Joshua Grierson <encredgaming@gmail.com>
2020-01-02 15:59:59 +0000

    [helpers] - added helpers

--- index.ts
+++ index.ts
@@ -2,4 +2,4 @@ export * from "./lib/models";
 export * from "./lib/services";
 export * from "./lib/controllers";
 export * from "./lib/error";
-export * from "./lib/helpers/redis";
+export * from "./lib/helpers";
--- lib/helpers/index.ts
+++ lib/helpers/index.ts
@@ -1,7 +1,7 @@
 import Redis, { RedisOptions } from "ioredis";
 import { RedisEventType } from "../models";
 
-export default async function setupRedis(options: RedisOptions): Promise<Redis.Redis> {
+export async function setupRedis(options: RedisOptions): Promise<Redis.Redis> {
     const redis: Redis.Redis = new Redis(options);
 
     const handler: (type: RedisEventType, err?: string) => void = (type: RedisEventType, err?: any) => {
